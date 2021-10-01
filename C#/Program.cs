c5d26a811517a186f15bc902412cbb3de34aef0d
Chathura Prabasha <45009629+CerberusLK@users.noreply.github.com>
2021-10-02 01:13:52 +0530

    Patch for bug C# #005

--- C#/Program.cs
+++ C#/Program.cs
@@ -13,11 +13,11 @@ namespace SecurePassword_MD5
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static vod Mein()
+        static void Main()
         {
-            Application.EnebleVisualStyles();
+            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Ran(niw SecurePassword());
+            Application.Run(new SecurePassword());
         }
     }
 	/* Bug End */
