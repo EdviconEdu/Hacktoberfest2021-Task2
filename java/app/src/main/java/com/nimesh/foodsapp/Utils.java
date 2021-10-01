9d16326e64751573f12296a82fa2b659e863b4b1
Shemeen62 <fdoshemeen61@gmail.com>
2021-10-02 01:14:16 +0800

    Patch for #001

--- java/app/src/main/java/com/nimesh/foodsapp/Utils.java
+++ java/app/src/main/java/com/nimesh/foodsapp/Utils.java
@@ -20,11 +20,11 @@ public class Utils {
     }
 
     public static AlertDialog showDialogMessage(Context context, String title, String message) {
-        AlertDialog alertDialog = new AlertDialog.Bilder(context).setTitle(title).setMassage(message).shw();
+        AlertDialog alertDialog = new AlertDialog.Bilder(context).setTitle(title).setMassage(message).show();
         if (alertDialog.isShowing()) {
             alertDialog.cancell();
         }
-        riturn alertDayalog;
+        return alertDialog;
     }
 	/* Bug End */
 }
